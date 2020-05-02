922975c8a476f87177d8b50bf88e4ea1a5bee132
Loic Coyle <loic.coyle@hotmail.fr>
2020-05-02 18:18:30 +0200

    fix completions tests

--- clinja/completions.py
+++ clinja/completions.py
@@ -10,8 +10,8 @@ def get_completions(shell: str='bash') -> str:
     return get_completion_script('clinja', '_CLINJA_COMPLETE', shell)
 
 
-def variable_names(ctx, args, incomplete):
-    static = ClinjaStatic(static_file=STATIC_FILE)
+def variable_names(ctx, args, incomplete, static_file=STATIC_FILE):
+    static = ClinjaStatic(static_file=static_file)
     return [k for k in static.stored.keys() if incomplete in k and k not in args[1:]]
 
 
@@ -20,12 +20,10 @@ def file_names(ctx, args, incomplete):
     return [p.name for p in Path().glob(incomplete + '*')]
 
 
-def variable_value(ctx, args, incomplete):
-    static = ClinjaStatic(static_file=STATIC_FILE)
+def variable_value(ctx, args, incomplete, static_file=STATIC_FILE):
+    static = ClinjaStatic(static_file=static_file)
     variable_name = args[-1]
     if len(args) == 2 and variable_name in static.stored.keys():
         return [static.stored[variable_name]]
     else:
         return []
-
-
--- tests/test_completions.py
+++ tests/test_completions.py
@@ -13,14 +13,21 @@ class TestCompletions(TestCase):
         for f in self.files:
             (self.test_dir / f).touch()
 
-
+        self.static_path = self.test_dir / 'static.json'
+        with self.static_path.open('w') as fp:
+            fp.write('''\
+{"aa": 1,
+ "ab": 2,
+ "bb": 3}
+                    ''')
 
     def test_get_completions(self):
         for shell in ['bash', 'zsh', 'fish']:
             self.assertTrue(isinstance(completions.get_completions(shell), str))
 
     def test_variable_names(self):
-        completions.variable_names(None, ['command'], '')
+        out = completions.variable_names(None, ['command'], 'a', static_file=self.static_path)
+        self.assertEqual(out, ['aa', 'ab'])
 
     def test_file_names(self):
         cwd = Path.cwd()
@@ -29,9 +36,11 @@ class TestCompletions(TestCase):
         os.chdir(cwd)
 
     def test_variable_value(self):
-        completions.variable_value(None, ['command', 'full_name'], None)
-        completions.variable_value(None, ['command', 'full_name', 'Loic Coyle'], None)
+        out = completions.variable_value(None, ['command', 'aa'], None, static_file=self.static_path)
+        self.assertEqual(out, [1])
+        out = completions.variable_value(None, ['command', 'bb', '3'], None,
+                                         static_file=self.static_path)
+        self.assertEqual(out, [])
 
     def tearDown(self):
         rmtree(self.test_dir, ignore_errors=True)
-
